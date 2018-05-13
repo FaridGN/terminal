637dce799b7a77d016742b1bf949633b890ea0ac
FaridGN <37265938+FaridGN@users.noreply.github.com>
2018-05-13 19:48:11 +0400

    Add files via upload

---
+++ e-manat/js/script.js
@@ -0,0 +1,14 @@
+
+function Bakcellopen() {
+    document.location="bakcell.html"
+}
+
+function clav(number) {
+ if(ipt.value.length<=8){
+    ipt.value+=number;
+    
+ }
+ else{
+     yoxla.style.display="block"
+ }
+}
