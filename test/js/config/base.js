7bad5358bfeb10384bab54359edfd8e15c96dd1f
FaridGN <37265938+FaridGN@users.noreply.github.com>
2018-05-24 00:15:58 +0400

    Add files via upload

---
+++ test/js/config/base.js
@@ -0,0 +1,13 @@
+
+let provider={
+    itemCount:9,
+    itemRows: 3,
+    itemWidth = "100px"
+}
+
+  document.body.appendChild(provider);
+
+
+
+
+
---
+++ test/js/config/script.js
@@ -0,0 +1,16 @@
+  let navbar = document.createElement("div");
+   navbar.className = "navig";
+   document.body.appendChild(navbar);
+
+  let logo = document.createElement("img");
+    logo.src = "images/logo.png";
+    logo.id = "mylogo";
+    document.body.appendChild(logo);
+
+  let nida = document.createElement("img");
+     nida.id = "exc";
+     nida.src = "images/nida.png";
+     document.body.appendChild(nida);
+
+   
+  
